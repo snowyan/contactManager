5edd4af647b600e4d505441f587aa6ad249492de
snowyan <eyonyan@gmail.com>
2020-07-08 11:04:31 +0200

    Updates

---
+++ precache-manifest.019cf2963afbf3a4b94fbdc0cf8a1840.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4f7f3fd36daf8885575d6028cf6e9a",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "b4811478c27c85dbda8e",
+    "url": "/contactManager/static/css/2.2e22b35d.chunk.css"
+  },
+  {
+    "revision": "622f8ebac5c2d5aa5bca",
+    "url": "/contactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b4811478c27c85dbda8e",
+    "url": "/contactManager/static/js/2.1cab0da6.chunk.js"
+  },
+  {
+    "revision": "84d1bff9c3a319468d0a3be925091b56",
+    "url": "/contactManager/static/js/2.1cab0da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622f8ebac5c2d5aa5bca",
+    "url": "/contactManager/static/js/main.a0ab834c.chunk.js"
+  },
+  {
+    "revision": "02bd93643b3897d34534",
+    "url": "/contactManager/static/js/runtime-main.4025dea3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contactManager/static/media/logo.5d5d9eef.svg"
+  }
+]);
